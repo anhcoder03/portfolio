e5849167a27db4ef46508270016ced72c5999bf8
anhcoder03 <nguyenphianh161003@gmail.com>
2023-05-15 11:58:07 +0700

    fix loading page

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -2,8 +2,12 @@
 
 const Loading = () => {
   return (
-    <div className="absolute inset-0 flex justify-center items-center">
-      <div className="w-10 h-10 border-4 border-t-orange-300 border-l-green-300 border-b-red-300 border-r-yellow-200 rounded-full animate-spin"></div>
+    <div className="min-h-screen flex justify-center items-center bg-black">
+      <div className="loader bg-white p-5 rounded-full flex space-x-3">
+        <div className="w-5 h-5 bg-primary rounded-full animate-bounce" />
+        <div className="w-5 h-5 bg-primary rounded-full animate-bounce" />
+        <div className="w-5 h-5 bg-primary rounded-full animate-bounce" />
+      </div>
     </div>
   );
 };
