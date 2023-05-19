24c737bf453156367658e318f8d7d25d7217633d
anhcoder03 <nguyenphianh161003@gmail.com>
2023-05-19 12:04:29 +0700

    [FixBug] router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { Suspense } from "react";
-import { useRoutes } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 import Loading from "./components/common/Loading";
-import routes from "./routers";
+
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const SkillPage = React.lazy(() => import("./pages/SkillPage"));
+const ProjectPage = React.lazy(() => import("./pages/ProjectPage"));
 
 function App() {
   return (
     <Suspense fallback={<Loading />}>
-      {useRoutes(routes())}
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/skill" element={<SkillPage />}></Route>
         <Route path="/project" element={<ProjectPage />}></Route>
-      </Routes> */}
+      </Routes>
     </Suspense>
   );
 }
--- src/routers/publicRoutes/index.tsx
+++
@@ -1,9 +0,0 @@
-import HomePage from "../../pages/HomePage";
-import ProjectPage from "../../pages/ProjectPage";
-import SkillPage from "../../pages/SkillPage";
-
-export default [
-  { path: "/", element: <HomePage /> },
-  { path: "/skill", element: <SkillPage /> },
-  { path: "/project", element: <ProjectPage /> },
-];
